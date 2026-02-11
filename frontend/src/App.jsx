e2812f28f9a9a5c3ec4b5480f33dfc19c9f796c6
yashk-digi17 <yashk@digi-prodigy.com>
2026-02-11 16:23:39 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import FileUpload from "./pages/FileUpload";
-console.log("test");
-let unusedVar = 123;
+
 export default function App() {
   return <FileUpload />;
+  console.log("test");
+let unusedVar = 123;
 }
 
